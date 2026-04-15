4b8606a48394b056e61e42cf37f851434071e375
barbaraanger <barbaraangern@icloud.com>
2026-04-15 14:34:37 -0300

    removes event handling

--- src/main/java/com/barbaraanger/cardforge/user/application/dto/UserOutput.java
+++
@@ -1,4 +0,0 @@
-package com.barbaraanger.cardforge.user.application.dto;
-
-public class UserOutput {
-}
--- src/main/java/com/barbaraanger/cardforge/user/application/port/out/UserEventPublisherPort.java
+++
@@ -1,7 +0,0 @@
-package com.barbaraanger.cardforge.user.application.port.out;
-
-import com.barbaraanger.cardforge.user.domain.UserCreatedEvent;
-
-public interface UserEventPublisherPort {
-    void publish(UserCreatedEvent userCreatedEventEvent);
-}
